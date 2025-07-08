31cebe71a54b00f1f0f65ec84a8179b25d72ab1b
anirudhsharma-2000 <anirudh16june@gmail.com>
2025-07-09 03:21:29 +0530

    added lazyColumn with effect

--- app/src/main/java/com/smitcoderx/scratch/MainActivity.kt
+++ app/src/main/java/com/smitcoderx/scratch/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.smitcoderx.scratch.ui.home.HomeScreen
+import com.smitcoderx.scratch.ui.home.CategoryScreen
 import com.smitcoderx.scratch.ui.theme.ScratchTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ScratchTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                    CategoryScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
--- app/src/main/java/com/smitcoderx/scratch/ui/home/CategoryScreen.kt
+++ app/src/main/java/com/smitcoderx/scratch/ui/home/CategoryScreen.kt
@@ -1,18 +1,14 @@
 package com.smitcoderx.scratch.ui.home
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -30,10 +26,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.smitcoderx.scratch.R
@@ -41,9 +45,10 @@ import com.smitcoderx.scratch.data.home.Categories
 import com.smitcoderx.scratch.data.home.Category
 import com.smitcoderx.scratch.ui.theme.Typography
 import kotlinx.coroutines.delay
+import java.lang.Integer.max
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun CategoryScreen(modifier: Modifier = Modifier) {
     val viewModel: CategoryViewModel = viewModel()
     val createdCategories = viewModel.categories.collectAsStateWithLifecycle().value
     val categories = Categories.entries
@@ -87,29 +92,55 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 )
             }
         }
-        val listState = rememberLazyListState()
+        val background = MaterialTheme.colorScheme.background
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(.85f)
+                .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+                .drawWithContent {
+                    drawContent()
+                    drawRect(
+                        brush = Brush.verticalGradient(
+                            colorStops = arrayOf(
+                                0.0001f to Color.Transparent,
+                                0.15f to background
+                            )
+                        ),
+                        blendMode = BlendMode.DstIn
+                    )
+                }
+                .weight(.85f),
+            verticalArrangement = Arrangement.spacedBy(-(60).dp)
         ) {
             itemsIndexed(createdCategories) { index, item ->
-                CategoryCard(item)
+                CategoryCard(item, index, createdCategories.size)
             }
         }
         if (openSheet) CategoryBottomSheet(viewModel, categories) { openSheet = false }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-private fun CategoryCard(category: Category) {
+private fun CategoryCard(category: Category, index: Int, size: Int) {
+    val rotation = if (index == 0) {
+        0f
+    } else {
+        val progress = index.toFloat() / max((size - 1), 1)
+        lerp(start = 0f, stop = 6f, fraction = progress)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp),
-        onClick = {},
+            .height(200.dp)
+            .zIndex((index + 1).toFloat())
+            .graphicsLayer {
+                rotationZ = rotation
+                transformOrigin = TransformOrigin(1f, 0.5f) // rotate from right edge
+            }
+            .padding(horizontal = rotation.dp),
         shape = RoundedCornerShape(30.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(category.color).copy(0.5f))
+        colors = CardDefaults.cardColors(containerColor = Color(category.color))
     ) {
         Row(
             modifier = Modifier
@@ -121,11 +152,6 @@ private fun CategoryCard(category: Category) {
                 modifier = Modifier.weight(.7f),
                 text = category.name,
                 style = Typography.displayLarge
-            )
-            Box(
-                modifier = Modifier
-                    .size(50.dp)
-                    .background(color = Color(category.color), shape = CircleShape)
             )
         }
     }
